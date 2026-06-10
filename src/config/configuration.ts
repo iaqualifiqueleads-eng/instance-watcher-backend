38828ddb540a69de3083ceefcd4412708ed74987
Gabriel Boldi <iaqualifiqueleads@gmail.com>
2026-06-10 15:17:38 -0300

    sei la

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,21 +1,37 @@
 import { config } from 'dotenv';
 import { ConfigService } from '@nestjs/config';
 
-config();
+// config();
 
-const configService = new ConfigService();
+// const configService = new ConfigService();
+
+// const envConfigs = {
+//   environment: configService.getOrThrow<string>('NODE_ENV'),
+//   port: parseInt(configService.getOrThrow<string>('PORT')),
+
+//   database: {
+//     url: configService.getOrThrow<string>('DATABASE_URL'),
+//     host: configService.getOrThrow<string>('DB_HOST'),
+//     port: configService.getOrThrow<string>('DB_PORT'),
+//     user: configService.getOrThrow<string>('DB_USER'),
+//     name: configService.getOrThrow<string>('DB_NAME'),
+//     password: configService.getOrThrow<string>('DB_PASSWORD'),
+//   }
+// };
+
+// export { envConfigs };
 
 const envConfigs = {
-  environment: configService.getOrThrow<string>('NODE_ENV'),
-  port: parseInt(configService.getOrThrow<string>('PORT')),
+  environment: process.env.NODE_ENV,
+  port: parseInt(process.env.PORT || '3000'),
 
   database: {
-    url: configService.getOrThrow<string>('DATABASE_URL'),
-    host: configService.getOrThrow<string>('DB_HOST'),
-    port: configService.getOrThrow<string>('DB_PORT'),
-    user: configService.getOrThrow<string>('DB_USER'),
-    name: configService.getOrThrow<string>('DB_NAME'),
-    password: configService.getOrThrow<string>('DB_PASSWORD'),
+    url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    name: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
   }
 };
 
